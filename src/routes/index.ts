c38e2b80e120d6dc66ff5c198c2cd8927abadfb5
Moises Jr <mdeosjr@gmail.com>
2025-03-08 19:01:21 -0300

    feat: add regions endpoints to main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import usersRouter from "./userRoutes";
+import regionsRouter from './regionRoutes';
 
 const router = Router();
 
 router.use('/api/users', usersRouter);
+router.use('/api/regions', regionsRouter);
 
 router.get("/health", (req, res) => {
   res.status(200).send("OK");
